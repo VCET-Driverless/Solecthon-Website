a28c2511ce459f9368ce387fa56f3888fe921dfc
Aditdev <adityalawate2004@gmail.com>
2022-12-01 08:33:49 +0530

    made some changes in commiter

--- commiter.py
+++ commiter.py
@@ -1,7 +1,14 @@
 from os import system as s
 # Infinite loop for infinite commits to git.
 while True:
-    msg = input("Commit message?\n")
+    k=1
+    while(k==1):
+        msg = input("Commit message?\n")
+        if(msg ==""):
+            k = 1
+        else:
+            k = 0
     s(f"git add .")
     s(f"git commit -m \"{msg}\"")
     s("git push")
+    print(msg)
