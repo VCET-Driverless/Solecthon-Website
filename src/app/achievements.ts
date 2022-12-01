c5e199569d7e90dc6ce9ddc8383d2c6fc6f790aa
Aditdev <adityalawate2004@gmail.com>
2022-12-01 08:29:46 +0530

    Modified achivements

--- src/app/achievements.ts
+++ src/app/achievements.ts
@@ -1,6 +1,6 @@
 import { achievement } from './type';
 
-export const achievements: achievement[] = [
+export const Achievements: achievement[] = [
   { name: 'achievement1', description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, vitae, ab optio autem officiis, corrupti molestias error iure aut perspiciatis sint nobis recusandae neque laudantium quod eius quidem praesentium? Adipisci perspiciatis necessitatibus est ad", points: 10 },
   { name: 'achievement2', description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, vitae, ab optio autem officiis, corrupti molestias error iure aut perspiciatis sint nobis recusandae neque laudantium quod eius quidem praesentium? Adipisci perspiciatis necessitatibus est ad", points: 20 },
   { name: 'achievement3', description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, vitae, ab optio autem officiis, corrupti molestias error iure aut perspiciatis sint nobis recusandae neque laudantium quod eius quidem praesentium? Adipisci perspiciatis necessitatibus est ad", points: 30 },
--- src/app/achievements/achievements.component.ts
+++ src/app/achievements/achievements.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { achievements } from  '../achievements';
+import { Achievements } from  '../achievements';
 @Component({
   selector: 'app-achievements',
   templateUrl: './achievements.component.html',
   styleUrls: ['./achievements.component.css']
 })
 export class AchievementsComponent implements OnInit {
-  ach = achievements;
+  achievements = Achievements;
   
   constructor() { }
 
