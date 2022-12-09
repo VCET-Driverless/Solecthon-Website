7d46260ea191c34e8e8a4bfe744d623d9c71b062
Aditdev <adityalawate2004@gmail.com>
2022-12-09 17:54:57 +0530

    Changes in team members name and image syntax

--- src/app/Team.ts
+++ src/app/Team.ts
@@ -3,36 +3,34 @@ export const Team: Member[] = [
   {
     name: 'Sara',
     role: 'Founder',
-    image: '../../assets/images/solecthon.jpg',
     description:
-      'Sara is a 3rd year student at the University of Waterloo studying Computer Science and Business. She is passionate about technology and its impact on society. She is also a huge fan of the outdoors and enjoys hiking, camping, and skiing. She is excited to be a part of the team and is looking forward to the future of the project.',
+      'Sara is a 3rd year student at the University of Waterloo ',
+  },
+  {
+    name: 'Sara',
+    role: 'Founder',    
+    description:
+      'Sara is a 3rd year student at the University of Waterloo ',
   },
   {
     name: 'Sara',
     role: 'Founder',
-    image: '../../assets/images/solecthon.jpg',
+    
     description:
-      'Sara is a 3rd year student at the University of Waterloo studying Computer Science and Business. She is passionate about technology and its impact on society. She is also a huge fan of the outdoors and enjoys hiking, camping, and skiing. She is excited to be a part of the team and is looking forward to the future of the project.',
+      'Sara is a 3rd year student at the University of Waterloo ',
   },
   {
     name: 'Sara',
     role: 'Founder',
-    image: '../../assets/images/solecthon.jpg',
+    
     description:
-      'Sara is a 3rd year student at the University of Waterloo studying Computer Science and Business. She is passionate about technology and its impact on society. She is also a huge fan of the outdoors and enjoys hiking, camping, and skiing. She is excited to be a part of the team and is looking forward to the future of the project.',
+      'Sara is a 3rd year student at the University of Waterloo ',
   },
   {
     name: 'Sara',
     role: 'Founder',
-    image: '../../assets/images/solecthon.jpg',
+    
     description:
-      'Sara is a 3rd year student at the University of Waterloo studying Computer Science and Business. She is passionate about technology and its impact on society. She is also a huge fan of the outdoors and enjoys hiking, camping, and skiing. She is excited to be a part of the team and is looking forward to the future of the project.',
-  },
-  {
-    name: 'Sara',
-    role: 'Founder',
-    image: '../../assets/images/solecthon.jpg',
-    description:
-      'Sara is a 3rd year student at the University of Waterloo studying Computer Science and Business. She is passionate about technology and its impact on society. She is also a huge fan of the outdoors and enjoys hiking, camping, and skiing. She is excited to be a part of the team and is looking forward to the future of the project.',
+      'Sara is a 3rd year student at the University of Waterloo ',
   },
 ];
