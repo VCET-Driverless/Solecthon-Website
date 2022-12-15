677b938155f941a3ca50a401425d702c82b3b207
Aditdev <adityalawate2004@gmail.com>
2022-12-15 11:18:45 +0530

    Contact us updated with Material components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {
+  MatFormFieldModule,
+  MatFormFieldAppearance,
+  MatFormFieldControl,
+} from '@angular/material/form-field';
 import { MatCardModule } from '@angular/material/card';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
@@ -30,11 +35,12 @@ import { HttpClientModule } from '@angular/common/http';
     ErrorComponent,
   ],
   imports: [
-    BrowserModule,
+    BrowserModule, MatFormFieldModule,
     AppRoutingModule,
     MatCardModule,
     FormsModule,
-    ReactiveFormsModule,
+    ReactiveFormsModule,    
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent],
